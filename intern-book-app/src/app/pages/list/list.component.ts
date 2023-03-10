4d804ff08c37e13a80fcc74665886c7239e9dc2c
AariyJP <aariyjp@aariy.net>
2023-03-10 11:46:21 +0900

    *ngForでモックデータを表示

--- intern-book-app/src/app/pages/list/list.component.ts
+++ intern-book-app/src/app/pages/list/list.component.ts
@@ -7,10 +7,8 @@ import { Book } from 'src/app/types/book';
   styleUrls: ['./list.component.scss']
 })
 export class ListComponent {
-
+  bookList: Book[] = [
+    { name: 'アンドロイドは電気羊の夢を見るか?', detail: '第三次大戦後の未来、サンフランシスコを舞台に賞金稼ぎのリック・デッカードが火星から逃亡してきた8体のアンドロイドを「処理」するというあらすじ', evaluation: 90 },
+    { name: '岩田さん: 岩田聡はこんなことを話していた。', detail: '任天堂の元社長、岩田聡さんのことばをまとめた本', evaluation: 90 }
+  ];
 }
-
-const bookList: Book[] = [
-  { name: 'アンドロイドは電気羊の夢を見るか?', detail: '第三次大戦後の未来、サンフランシスコを舞台に賞金稼ぎのリック・デッカードが火星から逃亡してきた8体のアンドロイドを「処理」するというあらすじ', evaluation: 90 },
-  { name: '岩田さん: 岩田聡はこんなことを話していた。', detail: '任天堂の元社長、岩田聡さんのことばをまとめた本', evaluation: 90 }
-];
