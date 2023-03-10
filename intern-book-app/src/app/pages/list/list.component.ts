8533bae20c5fcadfcadc89c2031cfb0960598b2c
AariyJP <aariyjp@aariy.net>
2023-03-10 11:40:30 +0900

    bookListを作成

--- intern-book-app/src/app/pages/list/list.component.ts
+++ intern-book-app/src/app/pages/list/list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Book } from 'src/app/types/book';
 
 @Component({
   selector: 'app-list',
@@ -8,3 +9,8 @@ import { Component } from '@angular/core';
 export class ListComponent {
 
 }
+
+const bookList: Book[] = [
+  { name: 'アンドロイドは電気羊の夢を見るか?', detail: '第三次大戦後の未来、サンフランシスコを舞台に賞金稼ぎのリック・デッカードが火星から逃亡してきた8体のアンドロイドを「処理」するというあらすじ', evaluation: 90 },
+  { name: '岩田さん: 岩田聡はこんなことを話していた。', detail: '任天堂の元社長、岩田聡さんのことばをまとめた本', evaluation: 90 }
+];
